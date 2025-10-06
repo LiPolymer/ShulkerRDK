f9706316658c0a9e7292c0bf95f4469128ae685f
LiPolymer <fish@lipoly.ink>
2025-10-06 11:51:03 +0800

    feat(ffmpeg): dependency resolution

--- ShulkerRDK.FFmpeg/Extension.cs
+++ ShulkerRDK.FFmpeg/Extension.cs
@@ -1,10 +1,16 @@
-﻿using FFMpegCore;
-using ShulkerRDK.Shared;
+﻿using ShulkerRDK.Shared;
 
 namespace ShulkerRDK.FFmpeg;
 
 public class Extension : ExtensionBase {
     public Extension() {
+        #if !DEBUG
+        NugetHelper.DependencyVerify("FFMpegCore/5.2.0","netstandard2.0");
+        NugetHelper.DependencyVerify("Instances/3.0.1","netstandard2.0");
+        NugetHelper.DependencyVerify("System.IO.Pipelines/10.0.0-preview.7.25380.108");
+        NugetHelper.DependencyVerify("System.Text.Json/10.0.0-preview.7.25380.108");
+        NugetHelper.DependencyVerify("System.Text.Encodings.Web/10.0.0-preview.7.25380.108","net8.0",false);
+        #endif
         LevitateMethods.Add("a2ogg",Convert.Method);
     }
     
