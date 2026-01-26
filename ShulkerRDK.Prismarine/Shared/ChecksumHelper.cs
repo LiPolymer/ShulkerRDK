6022feb187ba0a5e0819d39811376b8683de3b6d
LiPolymer <fish@lipoly.ink>
2026-01-26 20:59:02 +0800

    feat(pri): PriFile

---
+++ ShulkerRDK.Prismarine/Shared/ChecksumHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ShulkerRDK.Prismarine.Shared;
+
+public static class ChecksumHelper {
+    public static string GetChecksum(FileStream file) {
+        return GetSha1(file);
+    }
+
+    public static string GetChecksum(string str,Encoding? encoding = null) {
+        return GetStringSha1(str,encoding);
+    }
+
+public static string GetFileChecksum(string filePath) {
+        FileStream fs = new FileStream(filePath, FileMode.Open);
+        string cs = GetChecksum(fs);
+        fs.Close();
+        return cs;
+    }
+
+    static string GetSha1(FileStream file) {
+#pragma warning disable SYSLIB0021
+        SHA1 sha1 = new SHA1CryptoServiceProvider();
+#pragma warning restore SYSLIB0021
+        byte[] rawHash = sha1.ComputeHash(file);
+        StringBuilder sc = new StringBuilder();
+        foreach (byte t in rawHash) {
+            sc.Append(t.ToString("x2"));
+        }
+        return sc.ToString();
+    }
+    
+    static string GetStringSha1(string str, Encoding? encoding = null) {
+        encoding ??= Encoding.UTF8;
+        byte[] byteArray = encoding.GetBytes(str);
+        byte[] rawHash = SHA1.HashData(byteArray);
+        StringBuilder sc = new StringBuilder();
+        foreach (byte t in rawHash) {
+            sc.Append(t.ToString("x2"));
+        }
+        return sc.ToString();
+    }
+}
---
+++ ShulkerRDK.Prismarine/Shared/PrismarineFile.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using ReverseMarkdown.Converters;
+using ShulkerRDK.Shared;
+using Trident.Abstractions.Repositories.Resources;
+using Trident.Abstractions.Utilities;
+namespace ShulkerRDK.Prismarine.Shared;
+
+public class PrismarineFileMeta {
+    public PrismarineFileMeta(Package triPack) {
+        Purl = PackageHelper.ToPurl(triPack);
+        if (triPack.Sha1 != null) Sha1 = triPack.Sha1;
+    }
+    public PrismarineFileMeta(string purl) {
+        if (!PackageHelper.TryParse(purl, out (string Label, string? Namespace, string Pid, string? Vid) pdi))
+            throw new ArgumentException($"无法解析purl: [{purl}]");
+        Purl = PackageHelper.ToPurl(pdi.Label,pdi.Namespace,pdi.Pid,pdi.Vid); //标准化
+    }
+    public string Purl;
+    public string? Sha1;
+}
+
+public class PrismarineFileInstance {
+    public required string FilePath;
+    public required PrismarineFileMeta Meta;
+
+    public static PrismarineFileInstance Load(string path) {
+        return new PrismarineFileInstance {
+            FilePath = path,
+            Meta = JsonSerializer.Deserialize<PrismarineFileMeta>(File.ReadAllText(path)) 
+                   ?? throw new FileLoadException()
+        };
+    }
+
+    public static PrismarineFileInstance Create(string path, PrismarineFileMeta meta) {
+        PrismarineFileInstance pfi = new PrismarineFileInstance {
+            FilePath = path,
+            Meta = meta
+        };
+        pfi.Save();
+        return pfi;
+    }
+    
+    public void Save(string? path = null) {
+        path ??= FilePath;
+        string? dir = Path.GetDirectoryName(path);
+        if (dir != null & !Directory.Exists(dir)) Directory.CreateDirectory(dir!);
+        File.WriteAllText(path, JsonSerializer.Serialize(Meta, Tools.JsonSerializerOptions));
+    }
+}
